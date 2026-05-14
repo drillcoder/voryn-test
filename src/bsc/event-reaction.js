7d3ecbce55595b1d20b246ca331c3c2d13297149
drill <nitrodrill@ya.ru>
2026-05-14 19:56:35 +0300

    add bsc reactions

---
+++ src/bsc/event-reaction.js
@@ -0,0 +1,28 @@
+import {ConsoleLogger, EventReactionWorker} from "@drillcoder/voryn";
+
+const config = {
+    chainId: Number(process.env.BSC_CHAIN_ID),
+    delayBetweenTicksMs: 500,
+    workerName: "event-reaction-worker",
+    batchSize: 5_000,
+};
+
+const logger = new ConsoleLogger({minLevel: "info"});
+const dbUrl = process.env.DB_URL;
+const lockKey = 102n;
+
+const handler = {
+    async handle(event) {
+        logger.info("event_received", {
+            blockNumber: event.blockNumber,
+            index: event.index,
+        });
+    },
+};
+
+const worker = await EventReactionWorker.create({config, logger, dbUrl, lockKey, handler});
+
+process.once("SIGINT", () => process.exit(0));
+process.once("SIGTERM", () => process.exit(0));
+
+await worker.start();
---
+++ src/bsc/transaction-reaction.js
@@ -0,0 +1,28 @@
+import {ConsoleLogger, TransactionReactionWorker} from "@drillcoder/voryn";
+
+const config = {
+    chainId: Number(process.env.BSC_CHAIN_ID),
+    delayBetweenTicksMs: 500,
+    workerName: "transaction-reaction-worker",
+    batchSize: 1_000,
+};
+
+const logger = new ConsoleLogger({minLevel: "info"});
+const dbUrl = process.env.DB_URL;
+const lockKey = 101n;
+
+const handler = {
+    async handle(tx) {
+        logger.info("transaction_received", {
+            blockNumber: tx.blockNumber,
+            index: tx.index,
+        });
+    },
+};
+
+const worker = await TransactionReactionWorker.create({config, logger, dbUrl, lockKey, handler});
+
+process.once("SIGINT", () => process.exit(0));
+process.once("SIGTERM", () => process.exit(0));
+
+await worker.start();
