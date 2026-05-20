b8d16637501805a0339e26da0234974b2e7f4cf4
drill <nitrodrill@ya.ru>
2026-05-20 16:52:17 +0300

    add block job recovery functionality for Ethereum chain

---
+++ src/bsc/block-job-recovery.js
@@ -0,0 +1,19 @@
+import { BlockJobRecovery, ConsoleLogger } from "@drillcoder/voryn";
+
+const config = {
+    chainId: Number(process.env.BSC_CHAIN_ID),
+};
+const blockNumber = Number(process.argv[2]);
+
+const logger = new ConsoleLogger({ minLevel: "info" });
+const dbUrl = process.env.DB_URL;
+
+const recovery = await BlockJobRecovery.create({ config, logger, dbUrl });
+
+try {
+    const singleBlockResult = await recovery.retryFailedBlock(blockNumber);
+
+    console.log(JSON.stringify(singleBlockResult, null, 2));
+} finally {
+    await recovery.close();
+}
